21a0be50521a11d33d8901bad892cff00b3b192f
ssgenpower9554-creator <ssgenpower9554@gmail.com>
2026-05-11 21:20:58 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDePC2d62_Y9gmzqN2QzcQDU38ESJYIrmM",
@@ -12,4 +14,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
 export default app;
