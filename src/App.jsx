86168665988f9ee671c51ba4d9ed7c311870605c
ssgenpower9554-creator <ssgenpower9554@gmail.com>
2026-05-11 18:21:23 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{
+      background:"#071f18",
+      color:"#fff",
+      minHeight:"100vh",
+      padding:"40px",
+      fontFamily:"Arial"
+    }}>
+      <h1>S S GENPOWER</h1>
+
+      <p>
+        Premium React + Firebase Website Running Successfully
+      </p>
+    </div>
+  )
+}
+
+export default App
